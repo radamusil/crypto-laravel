c2a14c21ab99b2007e4c362d951dcde9a80a5d75
radamusil <64097743+radamusil@users.noreply.github.com>
2020-11-12 10:55:52 +0100

    React + Laravel starter

---
+++ resources/js/app/App/App.jsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+ 
+export default function App() {
+ 
+    return (
+        <h2>React App</h2>
+    )
+}
---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Laravel Crypto</h1>
+    <div id="app"><div>
+    <script src="{{ mix('js/app.js') }}"></script>
+@endsection
